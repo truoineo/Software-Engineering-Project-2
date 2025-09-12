b2827a6c55b00d1f371b7ec2355cc2b1dbe23a08
tahjOrona <123672740+tahjOrona@users.noreply.github.com>
2025-09-12 09:39:52 -0400

    Update MCP_server.py

--- MCP_server.py
+++ MCP_server.py
@@ -24,12 +24,10 @@ def list_tasks() -> List[dict]:
         return {"message: No task present"}
 
 @mcp.tool()
-def add_task(title: str, description: str = None, str = None) -> dict:
+def add_task(title: str, description: str = None, status: bool = False) -> dict:
     """Add a new task"""
-    payload = {"title": title}
+    payload = {"title": title, "status": status}
     if description is not None: payload["description"] = description
-    #if start_date is not None: payload["start_date"] = start_date
-    #if due_date is not None: payload["due_date"] = due_date
     try:
         resp = requests.post(f"{BACKEND_URL}/tasks", json=payload)
         resp.raise_for_status()
@@ -43,8 +41,6 @@ def update_task(task_id: int, title: str, description: str = None, status: bool 
     """Update an existing task"""
     payload = {"title": title, "status": status}
     if description is not None: payload["description"] = description
-    #if start_date is not None: payload["start_date"] = start_date
-    #if due_date is not None: payload["due_date"] = due_date
     try:
         resp = requests.put(f"{BACKEND_URL}/tasks/{task_id}", json=payload)
         resp.raise_for_status()
